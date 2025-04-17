ea0470aaf5da507fccdc1061828d67561644576b
Suhrob <suhrob@gmail.com>
2025-04-17 20:57:30 +0500

    t

--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,6 @@ window.addEventListener("load", function() {
     const user = JSON.parse(localStorage.getItem("user"));
     if(user === null) {
         console.log(this.window.location.href);
-        this.window.location.href = "./pages/home.html"
+        this.window.location.href = "http://127.0.0.1:5500/PAGES/home.html";
     }
 })

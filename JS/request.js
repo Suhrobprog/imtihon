e5b0ec33d7efcaa9400467460e60cdcdf9717c1d
Suhrob <suhrob@gmail.com>
2025-04-18 02:00:16 +0500

    tugadi

--- JS/request.js
+++ JS/request.js
@@ -15,3 +15,21 @@ export async function login(fn37) {
         throw new Error("Xatolik bo‘ldi");
     }
 }
+
+// export async function addcars(obj) {
+//     const req = await fetch("https://json-api.uz/api/project/fn37/auth/login", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(fn37),
+//     });
+
+//     const res = await req.json();
+
+//     if (req.status === 200) {
+//         return res;
+//     } else {
+//         throw new Error("Xatolik bo‘ldi");
+//     }
+// }

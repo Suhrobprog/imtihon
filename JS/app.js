cf8eb5e00cb07ee80fd093d23176550db381bec7
Suhrob <suhrob@gmail.com>
2025-04-18 02:22:54 +0500

    tugadi

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,19 @@
 import "./protector.js"
+import { addProduct } from "./request.js";
+
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", function(e){
+    const title = prompt("Mashinagizni nomini yozing xorzcha bu sayt testa", "Damas");
+    const sendData = {title };
+
+    addProduct(sendData).then((res) => {
+        console.log(res);
+        
+    }).catch((err)=>{
+        console.log(err);
+        
+    }).finally(()=>{
+
+    })
+})
--- JS/request.js
+++ JS/request.js
@@ -16,20 +16,21 @@ export async function login(fn37) {
     }
 }
 
-// export async function addcars(obj) {
-//     const req = await fetch("https://json-api.uz/api/project/fn37/auth/login", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(fn37),
-//     });
+export async function addProduct(obj) {
+    const req = await fetch("https://json-api.uz/api/project/fn37/cars/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "", 
+        },
+        body: JSON.stringify(obj),
+    });
 
-//     const res = await req.json();
+    const res = await req.json();
 
-//     if (req.status === 200) {
-//         return res;
-//     } else {
-//         throw new Error("Xatolik bo‘ldi");
-//     }
-// }
+    if (req.status === 200) {
+        return res;
+    } else {
+        throw new Error("Xatolik bo'ldi");
+    }
+}
